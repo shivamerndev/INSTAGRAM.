858c6ac42b2ef69c202070c9e140e142b3b3c35f
Shivam Kumar <shivam.ownwork@gmail.com>
2026-04-12 02:36:39 +0530

    Implement socket.io for real-time messaging

--- client/src/chat/components/ChatFooter.jsx
+++ client/src/chat/components/ChatFooter.jsx
@@ -1,15 +1,26 @@
-import { Image, Sticker } from "lucide-react"
+import { Image, SendIcon, Sticker } from "lucide-react"
+import { emitMsg } from "../io/Socket"
 
-const ChatFooter = () => {
+const ChatFooter = ({ state: { input, setInput } }) => {
+
 
     return (
         <div className="flex sticky bottom-0 py-4 items-center px-4 bg-zinc-950 border-t border-zinc-800">
             <button className="text-xl mr-2">😊</button>
-            <input
-                type="text"
-                placeholder="Message..."
-                className="flex-1 bg-zinc-900 text-white px-4 py-2 rounded-full outline-none placeholder-gray-400"
-            />
+
+            <div className="flex-1 text-white relative " >
+                <input
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                    className="w-full placeholder-pink-200 bg-zinc-800 h-full py-2 px-6 rounded-full outline-none"
+                    type="text"
+                    placeholder="Message..."
+                />
+                {input && <SendIcon onClick={() => {
+                    emitMsg("client", input)
+                    setInput("")
+                }} className="text-sky-400 cursor-pointer absolute right-4 top-2 text-base" />}
+            </div>
             <div className="flex gap-3 text-xl ml-4">
                 <button>< Image /></button>
                 <button> < Sticker /> </button>
---
+++ client/src/chat/io/Socket.js
@@ -0,0 +1,16 @@
+import { io } from 'socket.io-client';
+
+let socket;
+
+export const connectSocket =  () => {
+    socket = io("http://localhost:4000");
+    console.log("Socket connected")
+}
+
+export const reciveMsg = (event, callback) => {
+    socket.on(event, callback);
+}
+
+export const emitMsg = (event, msg) => {
+    socket.emit(event, msg);
+}
